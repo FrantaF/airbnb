ee35fb8159e9ed2b0dbfa38c46465e9fb66b32eb
FrantaF <frantafriedl@seznam.cz>
2018-08-14 12:45:37 +0800

    navbar button links to search box

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,19 @@
-
 # CarrierWave.configure do |config|
+
 #   config.permissions = 0666
 #   config.directory_permissions = 0777
-#   config.fog_provider = 'fog/awes'
-#   config.fog_credentials = {
-#     provider:              'AWS',                        
-#     aws_access_key_id:     'xxx',                        
-#     aws_secret_access_key: 'yyy',                        
-#     region:                'eu-west-1',                  
 
-#   }
-#   config.fog_directory  = 'name_of_bucket'                                      # required
-#   config.fog_public     = false                                                 # optional, defaults to true
-#   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+#   config.fog_provider = 'fog/aws'
+
+#   config.fog_credentials = {
+#     provider:              'AWS',
+#     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     region:                ENV['AWS_BUCKET_REGION']
+#  }
+
+#  config.fog_directory  = ENV['AMAZON_BUCKET_NAME']
+#  config.fog_public     = false
+
+#  config.storage = :fog
 # end
