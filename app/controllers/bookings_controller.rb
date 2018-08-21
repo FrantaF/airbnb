0cbab1f35c70ce4d5f83c5d82a6e76a40d01ff8c
FrantaF <frantafriedl@seznam.cz>
2018-08-21 22:07:18 +0800

    booking created after payment went successfully through

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,23 +3,23 @@ class BookingsController < ApplicationController
    before_action :require_login 
 
    def create      
-
+      byebug
+      params[:booking] = session["booking"]
       @booking = Booking.new(booking_params)
-      num_of_nights =  Date.parse(params[:booking][:end_date]) - Date.parse(params[:booking][:start_date])
-      price_per_night = Listing.find(params[:id]).price_per_night
-      @booking.total_price = num_of_nights * price_per_night
+      
+      
       
       if @booking.save
          # PostmanWorker.perform_later(current_user.id, params[:listing_id], @booking.id)
 
-         #pass on the booking id for retrieval of payment info
-         session["booking_id"] = @booking.id
-         redirect_to braintree_new_path(current_user.id)         
+         #pass on the booking id for retrieval of payment info         
+         redirect_to :root, :flash => { :success => "Transaction successful!" }         
       else
          # @listing = Listing.find(@booking.listing_id)
          # @errors = @booking.errors.full_messages
          # render 'listings/show'
          # redirect_to user_listing_path(@booking.user_id, @booking.listing_id)
+         redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
       end
    end
 
@@ -41,7 +41,7 @@ class BookingsController < ApplicationController
    private
 
    def booking_params
-      params.require(:booking).permit(:user_id, :listing_id, :start_date, :end_date)
+      params.require(:booking).permit(:user_id, :listing_id, :start_date, :end_date :total_price)
    end
 
 end
--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,24 +1,27 @@
+require 'date'
 class BraintreeController < ApplicationController
   before_action :require_login 
 
-  def new
-
+  def new        
     #only make this page accessable to the current user
-    if params[:id].to_i != current_user.id      
+    if params[:booking][:user_id].to_i != current_user.id      
       redirect_to listings_search_box_path
     else
       @client_token = Braintree::ClientToken.generate
+      num_of_nights =  Date.parse(params[:booking][:end_date]) - Date.parse(params[:booking][:start_date])
+      price_per_night = Listing.find(params[:booking][:listing_id]).price_per_night
+      total_price = (num_of_nights * price_per_night).to_i
+      params[:booking][:total_price] = total_price
+      session["booking"] = params[:booking]
       render "payment"
     end
 
   end
 
-  def checkout
+  def checkout    
 
-    #retrieve booking id from session 
-    payable_amount = Booking.find(session["booking_id"]).total_price.to_i
-    session["booking_id"] = nil
-    
+    payable_amount = session["booking"]["total_price"].to_i
+
     nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
 
     result = Braintree::Transaction.sale(
@@ -29,8 +32,8 @@ class BraintreeController < ApplicationController
       }
       )
 
-    if result.success?    
-      redirect_to :root, :flash => { :success => "Transaction successful!" }
+    if result.success?   
+      redirect_to create_booking_path(params[:id])
     else
      redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
    end
